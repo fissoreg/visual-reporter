f483d690df5f8236405e502ef2a2ac773b9c80f9
Giancarlo Fissore <giancarlo.fissore@gmail.com>
2017-11-21 12:36:43 +0100

    Addressed TODOs and finalized things. To test.

--- reporter.jl
+++ reporter.jl
@@ -1,13 +1,16 @@
+#module VisualReporter
+
 using Boltzmann
 using Plots, Images
 using Stats
-using Boltzmann
-using MNIST_utils
+
+# importing function to extend
+import Boltzmann.report
 
 # TODO:
 #  - define constructor (initializing plots in it) - Done!
 #  - implement preprocessing - Done!
-#  - eval() should probably be removed, using a Dict
+#  - eval() should probably be removed, using a Dict - Done!
 #  - how to pass Plots arguments to plot()? - SOLVED: splat Dict after semicolon, arg names being symbols
 
 struct VisualReporter <: Boltzmann.BatchReporter
@@ -16,28 +19,14 @@ struct VisualReporter <: Boltzmann.BatchReporter
   plots::Array{Dict{Symbol,Any},1}
 end
 
-pre = Dict(
-  :in => [:W],
-  :preprocessor => svd,
-  :out => [:U, :s, :V]
-)
+function get_args(rbm::AbstractRBM, args::Dict{Symbol,Any}...; pre=Dict())
+  new_args = isempty(pre) ? pre : preprocessing(pre, args)
+  merge(Dict(:W => rbm.W, :vbias => rbm.vbias, :hbias => rbm.hbias), new_args, args...)
+end
 
-p1 = Dict(
-  :ys => [:X],
-  :transforms = [x -> samplesToImg(generate(x[:,1:100], n_gibbs=15))],
-  :title = "Sampling"
-)
-
-p2 = Dict(
-  :ys => [:U, :V],
-  :transforms => [mean, mean],
-  :labels => ["U", "V"],
-  :title => "Means",
-  :incremental => true
-)
-
-function get_args(rbm::AbstractRBM, args::Dict{Symbol,Any}...)
-  merge(Dict(:W => rbm.W, :vbias => rbm.vbias, :hbias => rbm.hbias), args...)
+function get_plot_args(plot)
+  # NOTE: the array of Symbols should be saved somewhere...
+  filter((k,v) -> !(k in [:ys, :transforms, :incremental, :plot]), plot)
 end
 
 function y_values(args, ys)
@@ -51,11 +40,11 @@ end
 function plot_final(plot, args)
   ys = y_values(plot[:ys])
   transformed = apply_transforms(plot, ys)
-  plot[:plot] = plot(apply_transform(plot, ys))
+  plot[:plot] = plot(apply_transform(plot, ys), get_plot_args(plot))
 end
 
 function init_plot_incremental(plot, args)
-  plot[:plot] = plot(0, apply_transforms(plot, ys))
+  plot[:plot] = plot(0, apply_transforms(plot, ys), get_plot_args(plot))
 end
 
 function update_plot_incremental(plot, args)
@@ -71,9 +60,7 @@ function preprocessing(pre::Dict{Symbol,Any}, args::Dict{Symbol,Any})
 end
 
 function VisualReporter(rbm::AbstractRBM, every::Int, pre::Dict{Symbol,Any}, plots::Array{Dict{Symbol,Any},1}, init::Dict{Symbol,Any})
-  args = get_args(rbm, init)
-  new_args = preprocessing(pre, args)
-  args = merge(args, new_args)
+  args = get_args(rbm, init, pre=pre)
    
   for p in plots
     if haskey(plot, :incremental) && plot[:incremental]
@@ -85,18 +72,34 @@ function VisualReporter(rbm::AbstractRBM, every::Int, pre::Dict{Symbol,Any}, plo
 end
 
 function report(reporter::VisualReporter, rbm::AbstractRBM, epoch::Int, current_batch::Int, scorer::Function, X::Boltzmann.Mat, ctx::Dict{Any,Any})
-  
-  # aliases (make :W, :v and :h symbols available)
-  W = rbm.W
-  v = rbm.vbias
-  h = rbm.hbias
 
-  for plot in reporter.plots
-  end
+  #for plot in reporter.plots
+  #end
 end
+
+pre = Dict(
+  :in => [:W],
+  :preprocessor => svd,
+  :out => [:U, :s, :V]
+)
+
+p1 = Dict(
+  :ys => [:X],
+  :transforms => [x -> samplesToImg(generate(x[:,1:100], n_gibbs=15))],
+  :title => "Sampling"
+)
+
+p2 = Dict(
+  :ys => [:U, :V],
+  :transforms => [mean, mean],
+  :labels => ["U", "V"],
+  :title => "Means",
+  :incremental => true
+)
 
 X = randn(784, 2000)    # 2000 observations (examples) #  with 100 variables (features) each
 X = (X + abs(minimum(X))) / (maximum(X) - minimum(X)) # scale X to [0..1]
 rbm = GRBM(784, 50)     # define Gaussian RBM with 100 visible (input) 
                         #  and 50 hidden (output) variables
-fit(rbm, X, reporter=VisualReporter(1,exec))        
+vr = VisualReporter(10, pre, [p1, p2])
+fit(rbm, X, reporter=vr)        

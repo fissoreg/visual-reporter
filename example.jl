27c1c36933e497de9ea5a1bdf94888c9e7b178e2
Giancarlo Fissore <giancarlo.fissore@gmail.com>
2017-12-21 10:57:38 +0100

    Added some plots. To clean!

--- example.jl
+++ example.jl
@@ -2,11 +2,14 @@ using Boltzmann
 using MNIST
 
 include("reporter.jl")
+include("../mf-rbm/mf.jl")
 
 X, y = testdata()  # test data is smaller, no need to downsample
-X = X .* 2 ./ (maximum(X) - minimum(X)) - 1
+X = 2(X  ./ (maximum(X) - minimum(X))) - 1
 
-rbm = IsingRBM(28*28, 100; TrainData=X)
+rbm = IsingRBM(28*28, 100; sigma=0.001, X=X)
 
 vr = default_reporter(rbm, 100, X)
-fit(rbm, X, n_epochs=50, lr=0.00001, batch_size=20, n_gibbs=5, randomize=true, reporter=vr)
+fit(rbm, X, n_epochs=500, lr=1e-6, batch_size=20, eps=1e-8, max_iter=1000, randomize=true, reporter=vr, init=Dict(:X => X))
+#sampler=tap_gradient,
+mp4(vr.anim, "log.mp4", fps=2)
--- reporter.jl
+++ reporter.jl
@@ -1,22 +1,14 @@
 using Boltzmann
 using Plots, Images
 using Stats
+using MNIST_utils
 
-# MNIST #############################################################
-using Images
+include("../mf-rbm/mf.jl")
 
-function samplesToImg(samples; c=10, r=10, h=28, w=28)
-  f = zeros(r*h,c*w)
-  for i=1:r, j=1:c
-    f[(i-1)*h+1:i*h,(j-1)*w+1:j*w] = reshape(samples[:,(i-1)*c+j],h,w)
-  end
-  w_min = minimum(samples)
-  w_max = maximum(samples)
-  λ = x -> (x-w_min)/(w_max-w_min)
-  map!(λ,f,f)
-  colorview(Gray,f)
-end
-######################################################################
+# font settings
+f = Plots.font("Helvetica",4)
+fs = Dict(:guidefont => f, :legendfont => f, :tickfont => f)
+default(; fs...)
 
 # importing function to extend
 import Boltzmann.report
@@ -26,6 +18,7 @@ struct VisualReporter <: Boltzmann.BatchReporter
   pre::Dict{Symbol,Any}
   plots::Array{Dict{Symbol,Any},1}
   args::Dict{Symbol,Any}
+  anim::Animation
 end
 
 # NOTE: :preprocessor is unique while :tranforms are array;
@@ -52,7 +45,8 @@ end
 
 function apply_transforms(plot, ys)
   t = [typeof(x) <: Tuple ? f(x...) : f(x) for (f,x) in zip(plot[:transforms], ys)]
-  # WARNING: there's probably a more robust way...
+  # WARNING: there's probably a more robust way... Exactly!
+  # And that consists into using plot!()...
   if size(t)[1] == 1
     typeof(t[1]) <: Number ? t : t[1]
   else
@@ -110,19 +104,43 @@ function VisualReporter(rbm::AbstractRBM, every::Int, pre::Dict{Symbol,Any}, plo
   make_plots!(plots, args; init=true)
   plot(map(p -> p[:plot], plots)...)
   gui()
+  anim = Animation()
+  #frame(anim)
   println("Init done.")
   
-  VisualReporter(every, pre, plots, args)
+  VisualReporter(every, pre, plots, args, anim)
 end
 
 # activations
 activations = []
 
+# fixed points
+fps = []
+
+function preprop(rbm::RBM,X)
+  U,s,V = svd(rbm.W)
+  global fps = condensed_fps(rbm; fps=fps, params=Dict(:max_iter=>1000,:X=>X))
+  U,s,V,fps
+end
+
+function fps_to_img(fps)
+  # row size
+  row = 10
+  n = size(fps, 2)
+  println("# fps: ", n)
+
+  r = n > row ? div(n,row) : 1
+  c = n > row ? row : n
+  range = 1:(n > row ? r*row : n)
+  
+  samplesToImg(fps[:,range], r=r, c=c)
+end
+
 function default_reporter(rbm::AbstractRBM, every::Int, X)
   pre = Dict(
-    :in => [:W],
-    :preprocessor => svd,
-    :out => [:U, :s, :V]
+    :in => [:rbm,:X],
+    :preprocessor => preprop,
+    :out => [:U, :s, :V, :fps]
   )
   
   p1 = Dict(
@@ -195,7 +213,7 @@ function default_reporter(rbm::AbstractRBM, every::Int, X)
   )
   
   function push_activations!(rbm::AbstractRBM, samples::Array{T,2}) where T
-    m = mean(Boltzmann.sample_hiddens(rbm, samples)[1], 2)
+    m = mean(Boltzmann.sample_hiddens(rbm, samples), 2)
     global activations = length(activations) == 0 ? m : hcat(activations, m)
     activations
   end
@@ -207,7 +225,49 @@ function default_reporter(rbm::AbstractRBM, every::Int, X)
     :title => "Activations"
   )
   
-  VisualReporter(rbm, 100, pre, [p6, p1, p2, p3, p4, p5, p8, p7, p9], init=Dict(:X => X))
+  # highest singular value (time-series)
+  was = []
+  variances = []
+  
+  function phase_diagram!(W::Array{T,2}, wa::Real) where T
+    push!(variances, var(W))
+    push!(was, wa)
+    (was ./ variances, 1 ./ variances)
+  end
+  
+  p10 = Dict(
+    :ys => [(:W, :s)],
+    :transforms => [(W,s) -> phase_diagram!(W, s[1])],
+    :seriestype => :scatter
+  )
+
+  function fp(rbm, X)
+    mv, mh = fp_iter(rbm, X[:,1:100], tap_v_fp, tap_h_fp; max_iter = 20)
+    samplesToImg(mv)
+  end
+
+  p11 = Dict(
+    :ys => [(:rbm, :X)],
+    :transforms => [fp],
+    :title => "Fixed points"
+  )
+
+  p12 = Dict(
+    :ys => [:fps],
+    :transforms => [fps -> size(fps, 2)],
+    :incremental => true,
+    :title => "# fixed points",
+    :leg => false
+  )
+
+
+  p13 = Dict(
+    :ys => [:fps],
+    :transforms => [fps_to_img],
+    :title => "Fixed points"
+  )
+
+  VisualReporter(rbm, 100, pre, [p6, p1, p2, p3, p4, p5, p8, p7, p9, p10, p11, p12, p13], init=Dict(:X => X))
 end
 
 function report(reporter::VisualReporter, rbm::AbstractRBM, epoch::Int, current_batch::Int, scorer::Function, X::Boltzmann.Mat, ctx::Dict{Any,Any})
@@ -217,4 +277,5 @@ function report(reporter::VisualReporter, rbm::AbstractRBM, epoch::Int, current_
   make_plots!(reporter.plots, reporter.args)
   plot(map(p -> p[:plot], reporter.plots)...)
   gui()
+  #frame(reporter.anim)
 end
